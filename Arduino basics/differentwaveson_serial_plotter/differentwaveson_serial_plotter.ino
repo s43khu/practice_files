c8dc0c9ac56a26e0d261ac3ae36062559c841551
ABHISHEK (shekhu)☺️ <61787974+s43khu@users.noreply.github.com>
2022-06-06 12:59:42 +0530

    Update differentwaveson_serial_plotter.ino

--- Arduino basics/differentwaveson_serial_plotter/differentwaveson_serial_plotter.ino
+++ Arduino basics/differentwaveson_serial_plotter/differentwaveson_serial_plotter.ino
@@ -5,19 +5,12 @@ void loop() {
    // Uncomment one of the wave types below to display in Serial Plotter.
    // Serial Plotter window may need to be closed and reopened between runs
    // to remove old plot data.
-   // plotCombinedWaves();
    // plotSawtoothWave();
    plotSineWave();
    // plotSquareWave();
    // plotTriangleWave();
 }
-void plotCombinedWaves() {
-   for (float i = 0.0; i <= 2 * PI; i += 0.1) {
-      Serial.print(cos(i));  Serial.print(" ");  // cosine wave
-      Serial.print(sin(i));  Serial.print(" ");  // sine wave
-      Serial.println(i <= PI ? -1.5 : 1.5);      // square wave
-   }
-}
+
 void plotSawtoothWave() {
    for (int i = 0; i <= 100; i++) {
       Serial.println(i);
