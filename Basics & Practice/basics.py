2ed7f37dfb3f153c7f0ab3a4fb46ccd51f3baf10
ABHISHEK (shekhu)☺️ <61787974+s43khu@users.noreply.github.com>
2022-06-07 12:23:00 +0530

    Update basics.py

--- Basics & Practice/basics.py
+++ Basics & Practice/basics.py
@@ -1,16 +1,16 @@
-# TO FIND AVERAGE#---------------------------------->
+# TO FIND AVERAGE#-------------------------------------------->
 a = float(input("enter first value"))
 b = float(input("enter second value"))
 c = (a + b) / 2
 print("average of given two no. is=", c)
 
-# TO FIND THE VALUE OF POWER NO.#------------------>
+# TO FIND THE VALUE OF POWER NO.#---------------------------->
 d = float(input("enter no."))
 f = int(input("enter power"))
 e = d ** f
 print(e)
 
-# TO COMPARE TWO NO.#------------------------------>
+# TO COMPARE TWO NO.#--------------------------------------->
 n1 = float(input("enter 1st no."))
 n2 = float(input("enter 2nd no."))
 if n1 > n2:
